14d318b60146d2133835e660e29f68be886f16c5
Clément Dechaene <dechaene.clement@gmail.com>
2016-02-28 00:01:20 +0200

    Viewer

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,12 @@
 package main;
 
+import views.MainMenu;
+import views.SudokuViewer;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+       // new SudokuViewer("809000005000058000050006800068210300390000081001093740005100030000340000200000507:839721465672458193154936872468217359397564281521893746985172634716345928243689517");
+        new MainMenu();
     }
 }
